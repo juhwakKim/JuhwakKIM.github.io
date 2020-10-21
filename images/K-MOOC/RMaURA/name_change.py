57c98a2be131c7728fa1bc35dd353f6370ec7d67
juhwakKim <46366467+juhwakKim@users.noreply.github.com>
2020-10-22 02:07:34 +0900

    Site updated: 2020-10-22 02:07:31

--- images/K-MOOC/RMaURA/name_change.py
+++ images/K-MOOC/RMaURA/name_change.py
@@ -1,7 +1,14 @@
 import os
 
+print(os.listdir("./"))
 
-for i in range(2,18):
-    name = "Mask_RCNN_tutorial ({})".format(i)
-    new_name = "Mask_RCNN_tutorial_({})".format(i)
-    os.rename(name, new_name)
+for i in (os.listdir("./")):
+    new_name = i[:]
+    print(i[i.find(" ")])
+    if(i.find(" ") > 0):
+        new_name = new_name.replace(" ","_")
+        os.rename(i,new_name)
+# for i in range(2,18):
+#     name = "Mask_RCNN_tutorial ({})".format(i)
+#     new_name = "Mask_RCNN_tutorial_({})".format(i)
+#     os.rename(name, new_name)
